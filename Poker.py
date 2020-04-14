4c678611948dd6fea57cebcde7591bdf8c5dd509
Darcy Knox <darcy@cloudcannon.com>
2020-04-15 01:16:51 +1200

    Edge cases and input validation

--- Poker.py
+++ Poker.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import time
 
 class Poker:
     """docstring for Poker."""
@@ -10,6 +11,8 @@ class Poker:
 
 
 def main():
+
+
 
     suits = ["C", "D", "H", "S"]
 
@@ -41,10 +44,14 @@ def main():
 
     for line in sys.stdin:
 
+        start = time.time()
+
         finalString = ""
-        valid = True
+        validHand = True
 
         # get line of input and convert to an array of cards (hand)
+        inputLine = line.strip()
+
         line = line.strip().upper()
 
         hand = line.split("/")
@@ -54,17 +61,17 @@ def main():
             if (len(hand) == 1):
                 hand = line.split(" ")
                 if (len(hand) != 5):
-                    print("Invalid" + line)
-                    valid = False
+                    print("Invalid: " + inputLine)
+                    validHand = False
             elif (len(hand) != 5):
-                print("Invalid" + line)
-                valid = False
+                print("Invalid: " + inputLine)
+                validHand = False
         elif (len(hand) != 5):
-            print("Invalid: " + line)
-            valid = False
+            print("Invalid: " + inputLine)
+            validHand = False
 
 
-        if (valid):
+        if (validHand):
 
             hand = [card.replace("10", "T") for card in hand]
             hand = [card.replace("11", "J") for card in hand]
@@ -79,32 +86,47 @@ def main():
 
             handDict = {}
             for card in hand:
-                #print(handDict)
-                #print(card)
+                validCard = True
                 if card in handDict:
-                    print("duplicate card")
-                    print("Invalid: " + line)
+                    print("duplicate card: " + card)
+                    print("Invalid: " + inputLine)
+                    validCard = False
+                    validHand = False
                     break
                 elif not card in valuesDict:
-                    print("duplicate card")
-                    print("Invalid: " + line)
+                    print("Invalid: " + inputLine)
+                    validCard = False
+                    validHand = False
                     break
 
-                cardVal = valuesDict[card]
-                handDict.update({card : cardVal})
+                validCardPattern = re.compile(r'^([1-9]|(10|11|12|13)|[TJQKA])[CDHS]$')
+                validCard = validCardPattern.match(card)
 
-            #print(handDict)
-            sortedHand = sorted(handDict.items(), key= lambda cardValue: cardValue[1])
+                if validCard:
 
-            #print(sortedHand)
+                    cardVal = valuesDict[card]
+                    handDict.update({card : cardVal})
+
+                else:
+                    validHand = False
+                    break
 
 
-            for card in sortedHand:
-                finalString = finalString + card[0] + " "
+            if (validHand):
+                #print(handDict)
+                sortedHand = sorted(handDict.items(), key= lambda cardValue: cardValue[1])
 
-            finalString = finalString.strip().replace("T", "10")
+                #print(sortedHand)
 
-            print(finalString)
+
+                for card in sortedHand:
+                    finalString = finalString + card[0] + " "
+
+                finalString = finalString.strip().replace("T", "10")
+
+                print(finalString)
+
+            #print(time.time() - start)
 
 
 
